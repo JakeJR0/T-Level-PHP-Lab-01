4109b2c18e9bbc45d86b77fa26f2d4fb826c56ae
JakeJR0 <1453194@stu.tmc.ac.uk>
2022-09-22 18:37:55 +0100

    Adding constants for metatags

--- site_info.php
+++ site_info.php
@@ -1,9 +1,17 @@
 <?php
+# Site Information for Page
+
 define("SITE_NAME", "Student Viewer");
 define("SITE_DESCRIPTION", "This is a page created to view the age of students within the class.");
 define("COURSE_NAME", "T-Level Digital, Design and Production");
 define("COURSE_URL", "https://www.tmc.ac.uk/courses/industry-excellence-academy-t-level-digital-production-design-and-development-partnership/");
 define("COLLEGE_NAME", "The Manchester College");
 define("COLLEGE_URL", "https://www.tmc.ac.uk/");
+
+# Site Details for Meta Tags
+
+define("SITE_URL", $_SERVER['HTTP_HOST']);
+define("SITE_COLOUR", "#000000");
+
 ?>
 
