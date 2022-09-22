1cc51e6f25d770688b1a0f8b285b2afa9d11589f
JakeJR0 <1453194@stu.tmc.ac.uk>
2022-09-22 18:05:02 +0100

    Adding information to file

--- site_info.php
+++ site_info.php
@@ -1,3 +1,7 @@
 <?php
-    
+define("SITE_NAME", "Student Viewer");
+define("SITE_DESCRIPTION", "This is a page created to view the age of students within the class.");
+define("COURSE_NAME", "T-Level Digital, Design and Production");
+define("COLLEGE_NAME", "The Manchester College");
 ?>
+
