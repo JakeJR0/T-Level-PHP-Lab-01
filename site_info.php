0d741a9f6c9b9a5777fcf8a3f53dd0035d4af8b4
JakeJR0 <1453194@stu.tmc.ac.uk>
2022-09-22 18:24:12 +0100

    Adding Links to the file

--- site_info.php
+++ site_info.php
@@ -2,6 +2,8 @@
 define("SITE_NAME", "Student Viewer");
 define("SITE_DESCRIPTION", "This is a page created to view the age of students within the class.");
 define("COURSE_NAME", "T-Level Digital, Design and Production");
+define("COURSE_URL", "https://www.tmc.ac.uk/courses/industry-excellence-academy-t-level-digital-production-design-and-development-partnership/");
 define("COLLEGE_NAME", "The Manchester College");
+define("COLLEGE_URL", "https://www.tmc.ac.uk/");
 ?>
 
