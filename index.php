7259f96befa84859fd3016e9e67655ccc3604bdc
JakeJR0 <1453194@stu.tmc.ac.uk>
2022-09-22 18:48:35 +0100

    Adding Comments

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <html lang="en">
     <head>
         <?php
+            # Site Information for Page
             include 'site_info.php';
+
+            # Student Information
             include 'student_info.php';
         ?>
+
         <title>Student Viewer</title>
 
         <!-- Meta Tags -->
@@ -29,7 +33,7 @@
     
     </head>
     <body>
-        
+        <!-- Header -->
         <div style="margin-top: 30px;" class="text-center">
             <h1 class="display-4">
                 <?php echo SITE_NAME; ?>
@@ -38,7 +42,11 @@
                 <?php echo SITE_DESCRIPTION; ?>
             </p>
         </div>
+        <!-- End Header -->
+
+        <!-- Creates a container for the tables -->
         <div>
+            <!-- Creates a table collection -->
             <div class="table-collection">
                 <div class="table-container">
                     <h3 class="text-center">Unsorted Table</h3>
@@ -51,6 +59,7 @@
                         </thead>
                         <tbody>
                             <?php
+                                # Loops through the unsorted array and displays the data
                                 foreach ($students as $student) {
                                     echo "<tr>";
                                     foreach ($student as $value) {
@@ -73,7 +82,9 @@
                         </thead>
                         <tbody>
                             <?php
+                                # Sorts the array by name
                                 array_multisort(array_column($students, 'name'), SORT_ASC, $students);
+                                # Loops through the sorted array and displays the data
                                 foreach ($students as $student) {
                                     echo "<tr>";
                                     foreach ($student as $value) {
@@ -96,7 +107,9 @@
                         </thead>
                         <tbody>
                             <?php
+                                # Sorts the array by age
                                 array_multisort(array_column($students, 'age'), SORT_ASC, $students);
+                                # Loops through the sorted array and displays the data
                                 foreach ($students as $student) {
                                     echo "<tr>";
                                     foreach ($student as $value) {
@@ -120,24 +133,33 @@
 
         <div id="compare-table">
             <?php
+                # Checks if the user has submitted the form
                 if (isset($_GET["user"])) {
+                    # Gets the user input
                     $provided_name = $_GET["user"];
                 } else {
+                    # Sets the provided name to an empty string
                     $provided_name = "";
                 }
                 
                 $matched = false;
 
+                # Loops through the array 
                 foreach ($students as $student) {
+                    # Checks if the provided name matches
+                    # to one in the array
                     if ($student['name'] == $provided_name) {
+                        # Gets the student's name that matched
                         $student_age = $student['age'];
                         $matched = true;
                     }
                 }
                 
-                
-
+                # Checks if the provided name matched
+                # a name in the array and displays
+                # a response.
                 if ($matched == false) {
+                    # Checks if the user provided a name which was not a match or if the user did not provide a name.
                     if($provided_name == "") {
                         echo "<h3 class='text-center'>Please search for a person to compare</h3>";
                         echo "<h4 class='text-center' style='margin-bottom: 30px;'>Comparing by Average Age</h4>";
@@ -152,6 +174,7 @@
                 }
 
             ?>
+
             <table class="table">
                 <thead>
                     <tr>
@@ -176,6 +199,7 @@
                     ?>
                 </tbody>
             </table>
+            <!-- User Form -->
             <form action="" class="form-horizontal" style="margin-top: 50px;" align="center">
                 <div class="form-group">
                     <input type="text" name="user" placeholder="Name to compare" style="text-align: center">
@@ -194,18 +218,30 @@
             </thead>
             <tbody>
                 <?php
+                    # Loops through the array
                     foreach ($students as $student) {
+                        # Creates a table row
                         echo "<tr>";
+                        # Inserts the student name 
+                        # into the row
                         echo "<td>$student[name]</td>";
+
+                        # Checks if the student is 18 or over
+                        # and displays the correct status of the
+                        # user.
+
                         if ($student['age'] >= 18) {
                             echo "<td>Adult</td>";
                         } else {
                             echo "<td>Minor</td>";
                         }
+
+                        # Closes the table row
                         echo "</tr>";
                     }
                 ?>
         </table>
+        
         <!-- Footer (Shows College Name and Course name) -->
         <footer>
             <p class="text-center">College Name: <strong><?php echo '<a target="_blank" href="'. COLLEGE_URL .'">'. COLLEGE_NAME .'</a>'; ?></strong></p>
